e67e372026d607b5915756d6b3431bd75fda5aa8
kyle <kyle>
2015-05-15 19:56:52 +0000

    fix test app for better reading, fix test data

--- test/test.c
+++ test/test.c
@@ -8,19 +8,34 @@ void usage(){
   exit(1);
 }
 
+#ifndef ROT_TEST_MAX_BUF
+#define ROT_TEST_MAX_BUF 512
+#endif
+
 int main(int argc,
          char* argv[])
 {
   int rc;
   FILE *fin;
-  char input[512], expected[512];
+  char *input, *expected;
 
   if(argc < 2) usage();
 
+  input = malloc(ROT_TEST_MAX_BUF);
+  if(!input) return 1;
+  expected = malloc(ROT_TEST_MAX_BUF);
+  if(!expected) return 1;
+
   fin = fopen(argv[1], "r");
   if(!fin) return 1;
-  rc = fscanf(fin, "%s %s", &input, &expected);
-  if(rc != 2) return 1;
+  input = fgets(input, ROT_TEST_MAX_BUF, fin);
+  if(!input) return 1;
+  if(input[strlen(input)-1] == '\n')
+    input[strlen(input)-1] = '\0';
+  expected = fgets(expected, ROT_TEST_MAX_BUF, fin);
+  if(!expected) return 1;
+  if(expected[strlen(expected)-1] == '\n')
+    expected[strlen(expected)-1] = '\0';
   rc = rot13(input);
   if(rc) return 1;
   rc = strcmp(input, expected);
