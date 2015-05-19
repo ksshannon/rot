63c679cf8ade70a7bcdfd5255366781a3012d5f5
kyle <kyle>
2015-05-19 23:27:34 +0000

    library-fy rot

--- src/rot.h
+++ src/rot.h
@@ -7,11 +7,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+#if defined(_WIN32)
+#if defined(rot_EXPORTS)
+  #define ROT_DLL     __declspec(dllexport)
+#else
+  #define ROT_DLL     __declspec(dllimport)
+#endif /* rot_EXPORTS */
+#else
+  #define ROT_DLL
+#endif /* _WIN32 */
+
 #define strequal(a,b) strcmp(a,b)==0
 #define strnequal(a,b,n) strncmp(a,b,n)==0
 
-int rot(char* s, int rot);
-int rot13(char* s);
+ROT_DLL int rot(char* s, int rot);
+ROT_DLL int rot13(char* s);
 
 #endif /* ROT_H_ */
 
