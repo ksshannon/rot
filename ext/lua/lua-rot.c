9ec8140c4d5bb4eecf4dd040d25c83c7444869bd
kyle <kyle>
2015-05-20 00:19:33 +0000

    add lua code

---
+++ ext/lua/lua-rot.c
@@ -0,0 +1,37 @@
+
+#define LUA_LIB
+
+#include "rot.h"
+#include "lua.h"
+#include "lualib.h"
+#include "lauxlib.h"
+
+static int l_rot(lua_State *L){
+  char *p;
+  int rc, r, n;
+  const char *s;
+  n = lua_gettop(L);
+  if(!luaL_checkstring(L, 1, &s)){
+    lua_pushstring(L, "Invalid argument");
+    lua_error(L);
+  }
+  s = lua_tostring(L, 1);
+  r = 13;
+  if(nArgs > 1){
+    if(!luaL_isnumber(L,2)){
+      lua_pushstring(L, "Invalid argument");
+      lua_error(L);
+    }
+    r = lua_tonumber(L, 2);
+  }
+  p = strdup(s);
+  rc = rot(p, (int)r);
+  if(rc){
+    lua_pushstring(L, "Failed to rotate.");
+    lua_error(L);
+  }
+  lua_pushstring(L, p);
+  free(p);
+  return 1;
+}
+
