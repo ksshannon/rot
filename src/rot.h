afe614700d402099807868c20bd54ec7b34ce354
kyle <kyle>
2015-05-20 17:57:48 +0000

    allow c++ linkage

--- src/rot.h
+++ src/rot.h
@@ -17,11 +17,23 @@
   #define ROT_DLL
 #endif /* _WIN32 */
 
+#ifdef __cplusplus
+  #define ROT_C_START extern "C" {
+  #define ROT_C_END   }
+#else
+  #define ROT_C_START
+  #define ROT_C_END
+#endif
+
 #define strequal(a,b) strcmp(a,b)==0
 #define strnequal(a,b,n) strncmp(a,b,n)==0
 
+ROT_C_START
+
 ROT_DLL int rot(char* s, int rot);
 ROT_DLL int rot13(char* s);
+
+ROT_C_END
 
 #endif /* ROT_H_ */
 
