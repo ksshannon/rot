396c82ef785a3c9abd54c8dd0147934c7dba68aa
kyle <kyle>
2015-06-16 20:08:08 +0000

    reorder use of our string

--- ext/lua/luarot.c
+++ ext/lua/luarot.c
@@ -45,12 +45,12 @@ static int l_rot(lua_State *L)
     lua_isnumber(L,2);
     r = lua_tonumber(L, 2);
     p = malloc(strlen(s) + 1);
-    strcpy(p, s);
     if(!p)
     {
         lua_pushstring(L, "Failed to allocate for rotation.");
         lua_error(L);
     }
+    strcpy(p, s);
     rc = rot(p, (int)r);
     lua_pushstring(L, p);
     free(p);
